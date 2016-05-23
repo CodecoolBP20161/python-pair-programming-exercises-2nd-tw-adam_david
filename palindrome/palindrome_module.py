3d5e99c7ec8ae3b74791b692189039e7ab0f5a1c
Dávid Fehér <feherkeee@gmail.com>
2016-05-23 14:38:37 +0200

    palindrome_module.py finished

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,9 +1,15 @@
 def palindrome(str):
-    return
+    if str.lower().replace(" ", "") == str.lower()[::-1].replace(" ", ""):
+        return True
+    return False
 
 
 def main():
-    return
+    string = input("Enter a string: ")
+    if palindrome(string):
+        print("This is a palindrome!")
+    else:
+        print("This is not a palindrome!")
 
 
 if __name__ == '__main__':
